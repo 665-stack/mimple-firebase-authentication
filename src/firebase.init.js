26085945cf4695806fd086b76632332236a8d22f
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-03 13:57:54 +0600

    add full google authenticaion functionality

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA77RSz_W0BO6xuBXhQbe-kc_xcdE8DFKg",
+    authDomain: "mimple-firebase-authenti-c452a.firebaseapp.com",
+    projectId: "mimple-firebase-authenti-c452a",
+    storageBucket: "mimple-firebase-authenti-c452a.appspot.com",
+    messagingSenderId: "723804236231",
+    appId: "1:723804236231:web:1e281b46e9a3b89ff491e2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
